6b767a047fff67fc6a33ccc6fcee67b9192fcbed
Derek_DZ <a956997493@gmail.com>
2020-12-09 22:18:31 +0800

    全局引入reset.scss 初始化 el-button, el-input 修复el-input输入不显示的bug：this.$forceUpdate

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,36 @@
 <template>
-  <div>
-
+  <div id="app">
+    <el-input
+      @input="onChange"
+      type="text"
+      placeholder="请输入内容"
+      v-model="input">
+    </el-input>
+    <Buttons></Buttons>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import Buttons from '@/components/Buttons.vue';
 
 
 @Component({
   components: {
-
+    Buttons
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  input?: string
+  onChange(){
+    this.$forceUpdate()
+  }
+}
 </script>
 
 <style lang="scss">
+  @import "assets/style/reset.scss";
+  #app{
+    margin: 20px;
+  }
 </style>
---
+++ src/components/Buttons.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="wrapper">
+    <el-button class="submit" type="primary">提交</el-button>
+    <el-button class="clean">清空</el-button>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class NoName extends Vue {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+  .wrapper{
+    border: 1px solid red;
+    display: flex;
+    flex-direction: row;
+    align-items: end;
+    justify-content: end;
+    > .submit {
+      align-self: end;
+    }
+  }
+</style>
