9b21f8e6ccd37d081936b375ea8a47c32c9e14f2
Derek_DZ <a956997493@gmail.com>
2020-12-09 20:58:17 +0800

    安装 ts支持 vue-property-decorator 安装 element-ui 安装 element-ui按需加载 babel-plugin-component 配置 .babelrc

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import Vue from 'vue'
-import { Button, Select } from 'element-ui'
+// import { Button, Select } from 'element-ui'
 import App from './App.vue'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
-Vue.use(Button)
-Vue.use(Select)
+// Vue.use(Button)
+// Vue.use(Select)
 
 new Vue({
   render: h => h(App),
